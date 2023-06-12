d313b804dbd0ac05a9927dfbd017cd5579379b21
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-12 01:16:45 -0300

    colors

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ function App() {
 
   const quoteURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
 
+  const colorsArr = ['#4FC1FF', "#E8B9AB", '#CB769E', '#69995D', '#D2D7DF', '#3AA7A3', '#ECA400', '#006992', '#AFECE7', '#81F499', '#890620', '#B6465F', '#8ACDEA']
+
   const [quote, setQuote] = useState('Everything you’ve ever wanted is on the other side of fear.');
   const [author, setAuthor] = useState('George Addair');
   const [randomNumber, setRandomNumber] = useState(0);
   const [quotesArray, setQuotesArray] = useState(null);
+  const [accentColor, setAccentColor] = useState('#4FC1FF');
 
   
   const fetchQuotes = async (url) => {
@@ -26,9 +29,11 @@ function App() {
 
   const generateRandomNumber = () =>{ 
     let randomInterger = Math.floor(quotesArray.length * Math.random())
+    let randomIntergerColor = Math.floor(colorsArr.length * Math.random())
     setRandomNumber(randomInterger)
     setQuote(quotesArray[randomInterger].quote)
     setAuthor(quotesArray[randomInterger].author)
+    setAccentColor(colorsArr[randomIntergerColor])
   }
 
 
@@ -36,7 +41,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className='App-header'>
+      <header className='App-header' style={{backgroundColor:accentColor , color:accentColor}}>
         <div id='quote-box'>
 
         <p id='text'>
@@ -47,9 +52,9 @@ function App() {
          - {author}
          </p>
 
-      <button id='new-quote' onClick={() => generateRandomNumber()}>presionar</button>
+        <a id='tweet-quote' href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -${author}`)}>tweet Quote</a>
 
-        <a id='tweet-quote' href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -${author}`)}>tweet Quote</a> 
+        <button id='new-quote' onClick={() => generateRandomNumber()}>presionar</button>
         </div>
 
        
