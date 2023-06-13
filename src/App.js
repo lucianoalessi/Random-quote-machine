5c2906e17b5676c971ab6dd755f0e7dc240a5c6b
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-13 00:41:46 -0300

    fin

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 function App() {
 
 
-  const quoteURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+  const quoteURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"  //Array de objetos de quotes
 
-  const colorsArr = ['#4FC1FF', "#E8B9AB", '#CB769E', '#69995D', '#D2D7DF', '#3AA7A3', '#ECA400', '#006992', '#AFECE7', '#81F499', '#890620', '#B6465F', '#8ACDEA']
+  const colorsArr = ['#4FC1FF', "#E8B9AB", '#CB769E', '#69995D', '#D2D7DF', '#3AA7A3', '#ECA400', '#006992', '#AFECE7', '#81F499', '#890620', '#B6465F', '#8ACDEA']  //Array de colores
 
-  const [quote, setQuote] = useState('Everything you’ve ever wanted is on the other side of fear.');
-  const [author, setAuthor] = useState('George Addair');
-  const [randomNumber, setRandomNumber] = useState(0);
+  const [quote, setQuote] = useState('A person who never made a mistake never tried anything new.');    //iniciamos el estado de quote con el primer valor del array de quotes
+  const [author, setAuthor] = useState('Albert Einstein'); // lo mismo para el autor
+  const [randomNumber, setRandomNumber] = useState(0);  // iniciamos el estado del numero random con cero. 
   const [quotesArray, setQuotesArray] = useState(null);
-  const [accentColor, setAccentColor] = useState('#4FC1FF');
+  const [accentColor, setAccentColor] = useState('#4FC1FF'); //iniciamos el estado para el color de fondo con el primer color del array
 
   
   const fetchQuotes = async (url) => {
@@ -29,11 +29,11 @@ function App() {
   },[quoteURL])
 
 
-  const generateRandomNumber = () =>{ 
-    let randomInterger = Math.floor(quotesArray.length * Math.random())
-    let randomIntergerColor = Math.floor(colorsArr.length * Math.random())
-    setRandomNumber(randomInterger)
-    setQuote(quotesArray[randomInterger].quote)
+  const generateRandomNumber = () =>{                              
+    let randomInterger = Math.floor(quotesArray.length * Math.random())  // creamos un numero random acotado entre cero y el largo del array de quotes.
+    let randomIntergerColor = Math.floor(colorsArr.length * Math.random()) // lo mismo para el array de colores 
+    setRandomNumber(randomInterger) //cambiamos los estados
+    setQuote(quotesArray[randomInterger].quote) 
     setAuthor(quotesArray[randomInterger].author)
     setAccentColor(colorsArr[randomIntergerColor])
   }
@@ -57,11 +57,8 @@ function App() {
         </div>
         
         <div className='buttons'>
-          
          <a id='tweet-quote' href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -${author}`)} style={{backgroundColor:accentColor}}><FontAwesomeIcon icon={faTwitter}/></a>
-         
          <button id='new-quote' onClick={() => generateRandomNumber()} style={{backgroundColor:accentColor}}>New Quote</button>
-         
         </div>
 
       </div>
