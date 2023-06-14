423e502acbd89f2cae034baa9a5c0776ed1e6216
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-14 00:19:27 -0300

    finaliz

--- src/App.js
+++ src/App.js
@@ -42,8 +42,8 @@ function App() {
 
 
   return (
-    <body style={{backgroundColor:accentColor , color:accentColor}}>
-    <div className="App">
+
+    <div className="App" style={{backgroundColor:accentColor , color:accentColor}}>
 
         <div id='quote-box'   style={{color: accentColor}}>
           <h1 id='tittle'>Quote of day:</h1>
@@ -70,7 +70,6 @@ function App() {
         </div>
          
     </div>
-    </body>
   );
 }
 
