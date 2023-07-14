49e57fc5663234741b35df43b0eed4dec50970f7
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-07-14 20:25:37 -0300

    refactor(app.js): Improved code structure

--- src/App.js
+++ src/App.js
@@ -5,72 +5,91 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
 
-
-  const quoteURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"  //Array de objetos de quotes
-
   const colorsArr = ['#4FC1FF', "#E8B9AB", '#CB769E', '#69995D', '#D2D7DF', '#3AA7A3', '#ECA400', '#006992', '#AFECE7', '#81F499', '#890620', '#B6465F', '#8ACDEA']  //Array de colores
-
-  const [quote, setQuote] = useState('A person who never made a mistake never tried anything new.');    //iniciamos el estado de quote con el primer valor del array de quotes
-  const [author, setAuthor] = useState('Albert Einstein'); // lo mismo para el autor
-  const [randomNumber, setRandomNumber] = useState(0);  // iniciamos el estado del numero random con cero. 
-  const [quotesArray, setQuotesArray] = useState(null);
-  const [accentColor, setAccentColor] = useState('#4FC1FF'); //iniciamos el estado para el color de fondo con el primer color del array
-
   
+  //iniciamos el estado de quote con el primer valor del array de quotes y lo mismo para el autor.
+  const [quote, setQuote] = useState('A person who never made a mistake never tried anything new.'); 
+  const [author, setAuthor] = useState('Albert Einstein');
+  // iniciamos el estado del numero random con cero.
+  const [randomNumber, setRandomNumber] = useState(0);   
+  const [quotesArray, setQuotesArray] = useState(null);
+  //iniciamos el estado para el color de fondo con el primer color del array
+  const [accentColor, setAccentColor] = useState('#4FC1FF');
+
+  //API conArray de Json de quotes
+
+  const quoteURL = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"  
+
+  //Fetch form 1:
+
+  // fetch(quoteURL)
+  //   .then((response) => response.json())
+  //   .then( (parsedJSON) => {
+  //     setQuotesArray(parsedJSON.quotes)
+  //   })
+  
+
+  //Fetch form 2: 
+
   const fetchQuotes = async (url) => {
     const response = await fetch(url)
     const parsedJSON = await response.json()
     setQuotesArray(parsedJSON.quotes)
     console.log(parsedJSON)
   }
-
-  useEffect(()=>{
+  
+  
+ useEffect(()=>{
     fetchQuotes(quoteURL)
   },[quoteURL])
 
 
-  const generateRandomNumber = () =>{                              
-    let randomInterger = Math.floor(quotesArray.length * Math.random())  // creamos un numero random acotado entre cero y el largo del array de quotes.
-    let randomIntergerColor = Math.floor(colorsArr.length * Math.random()) // lo mismo para el array de colores 
-    setRandomNumber(randomInterger) //cambiamos los estados
+  const generateRandomNumber = () => {     
+    
+    // creamos un numero random acotado entre cero y el largo del array de quotes.Luego hacemos lo mismo para el array de colores.
+
+    let randomInterger = Math.floor(quotesArray.length * Math.random())  
+    let randomIntergerColor = Math.floor(colorsArr.length * Math.random())
+
+    //cambiamos los estados
+
+    setRandomNumber(randomInterger) 
     setQuote(quotesArray[randomInterger].quote) 
     setAuthor(quotesArray[randomInterger].author)
     setAccentColor(colorsArr[randomIntergerColor])
   }
 
 
-
-
   return (
     
     <html style={{backgroundColor:accentColor , color:accentColor}}>
-    <div className="App" >
+      <div className="App" >
 
         <div id='quote-box'   style={{color: accentColor}}>
           <h1 id='tittle'>Quote of day:</h1>
 
-        <div id='text'>
-          "{quote}"
-        </div>
+          <div id='text'>
+            "{quote}"
+          </div>
         
-        <div id='author'>
-          - {author}
-        </div>
+          <div id='author'>
+            - {author}
+          </div>
         
-        <div className='buttons'>
-         <a id='tweet-quote' href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -${author}`)} style={{backgroundColor:accentColor}}><FontAwesomeIcon icon={faTwitter}/></a>
-         <button id='new-quote' onClick={() => generateRandomNumber()} style={{backgroundColor:accentColor}}>New Quote</button>
+          <div className='buttons'>
+            <a id='tweet-quote' href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -${author}`)} style={{backgroundColor:accentColor}}><FontAwesomeIcon icon={faTwitter}/></a>
+            <button id='new-quote' onClick={() => generateRandomNumber()} style={{backgroundColor:accentColor}}>New Quote</button>
+          </div>
+
         </div>
 
-      </div>
-
-      <div className='author'>
+        <div className='author'>
           Designed and Coded by
           <br/>
           <a href='https://www.linkedin.com/in/lucianoalessi/' target='_blank'>Luciano A. Alessi</a>
         </div>
          
-    </div>
+      </div>
     </html>
   );
 }
